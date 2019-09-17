59ef8391485fb335a7babca13614c91f5eed53ea
Sakkeer Hssain <sakkeerhussainp@gmail.com>
2019-09-17 20:50:04 +0530

    Implemented cancel image loading

--- app/src/main/java/com/sakkeer/filesync/sample/CancelOptionActivity.kt
+++ app/src/main/java/com/sakkeer/filesync/sample/CancelOptionActivity.kt
@@ -3,23 +3,37 @@ package com.sakkeer.filesync.sample
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.sakkeer.filesync.client.FileSync
+import com.sakkeer.filesync.data.request.Request
 import kotlinx.android.synthetic.main.activity_one_image.*
 
 class CancelOptionActivity : AppCompatActivity() {
+
+    private lateinit var mRequest: Request
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_one_image)
 
+        setupViews()
+        setupListeners()
+    }
+
+    private fun setupViews() {
+
         val url = "http://images.unsplash.com/profile-1464495186405-68089dcd96c3?ixlib=rb-0.3.5\\u" +
                 "0026q=80\\u0026fm=jpg\\u0026crop=faces\\u0026fit=crop\\u0026h=128\\u0026w=128\\u0026" +
                 "s=622a88097cf6661f84cd8942d851d9a2"
 
-        FileSync
+        mRequest = FileSync
             .loadImage(url)
             // .addHeader("Authorization", "token goes here") // Add only if required
             .placeholder(R.drawable.placeholder)
             .errorImage(R.drawable.error_image)
             .toTarget(testImageView)
+
+    }
+
+    private fun setupListeners() {
+        mRequest.cancel()
     }
 }
