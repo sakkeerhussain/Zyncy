bea6f68e1214066d1501316221241ef9f240b773
Sakkeer Hussain <sakkeerhussainp@gmail.com>
2019-09-21 12:37:41 +0530

    Updated sample application code

--- app/src/main/java/com/sakkeer/filesync/sample/CancelOptionActivity.kt
+++ app/src/main/java/com/sakkeer/filesync/sample/CancelOptionActivity.kt
@@ -21,9 +21,7 @@ class CancelOptionActivity : AppCompatActivity() {
 
     private fun setupViews() {
 
-        val url = "http://images.unsplash.com/profile-1464495186405-68089dcd96c3?ixlib=" +
-                "rb-0.3.5\\u0026q=80\\u0026fm=jpg\\u0026crop=faces\\u0026fit=crop\\u0026h=128" +
-                "\\u0026w=128\\u0026s=622a88097cf6661f84cd8942d851d9a2"
+        val url = "https://images.unsplash.com/profile-1464495186405-68089dcd96c3"
 
         mRequest = FileSync
             .loadImage(url)
